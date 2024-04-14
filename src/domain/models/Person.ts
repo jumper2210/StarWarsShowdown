11e86656211117bf5a21d1abbc78447dce53404b
Krzysztof Skoczek <krzysztof@sintel.ie>
2024-04-14 15:38:39 +0200

    Add Person and Starship model

---
+++ src/domain/models/Person.ts
@@ -0,0 +1,12 @@
+export interface Person {
+  height: string;
+  mass: string;
+  hairColor: string;
+  skinColor: string;
+  eyeColor: string;
+  birthYear: string;
+  gender: string;
+  name: string;
+  homeworld: string;
+  url: string;
+}
---
+++ src/domain/models/Starship.ts
@@ -0,0 +1,15 @@
+export interface Starship {
+  model: string;
+  starshipClass: string;
+  manufacturer: string;
+  costInCredits: string;
+  length: string;
+  crew: string;
+  passengers: string;
+  maxAtmospheringSpeed: string;
+  hyperdriveRating: string;
+  MGLT: string;
+  cargoCapacity: string;
+  consumables: string;
+  name: string;
+}
